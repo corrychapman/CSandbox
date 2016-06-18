a884c5d6f326e7feb4029aaaae6f85e80c6f4b4a
Corry Chapman <corrychapman@macbook-pro-2.home>
2016-06-17 20:12:34 -0400

    Initial Commit

---
+++ Sandbox/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Sandbox
+//
+//  Created by Corry Chapman on 6/17/16.
+//  Copyright © 2016 CDev. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
